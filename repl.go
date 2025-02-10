e70b3d510926c8e9955ae629b263ce3b33db5e9e
uskocoglu <utkukocoglu@gmail.com>
2025-02-10 22:13:32 +0300

    help and exit commands implemented. Works fine but need better design

--- repl.go
+++ repl.go
@@ -7,6 +7,12 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name 		string
+	description string
+	callback 	func() error 
+} 
+
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -19,7 +25,15 @@ func startRepl() {
 
 		firstWord := words[0]
 
-		fmt.Println("Your command was: " + firstWord)
+		val, ok := aMap[firstWord]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := val.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
 
@@ -27,4 +41,18 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+	for key, value := range aMap {
+		fmt.Println(key + ": " + value.description)
+	}
+	return nil
 }
